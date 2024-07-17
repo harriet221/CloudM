f2601182a6f047126da1a62a07a01c7a5b4144d2
harriet221 <potter19@dongguk.edu>
2024-07-17 23:02:06 +0900

    08. @RequiredArgsConstructor 사용하여 생성자 메서드 생략

--- src/main/java/com/ysj/cloudm/domain/monologue/controller/MonologueController.java
+++ src/main/java/com/ysj/cloudm/domain/monologue/controller/MonologueController.java
@@ -3,19 +3,17 @@ package com.ysj.cloudm.domain.monologue.controller;
 import com.ysj.cloudm.domain.monologue.entity.Monologue;
 import com.ysj.cloudm.domain.monologue.service.MonologueService;
 import com.ysj.cloudm.global.rs.RsData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/monologue")
 public class MonologueController {
     private final MonologueService monologueService;
-
-    public MonologueController(MonologueService monologueService) {
-        this.monologueService = monologueService;
-    }
 
     @GetMapping("/play")
     String createMonologue() {
--- src/main/java/com/ysj/cloudm/domain/monologue/service/MonologueService.java
+++ src/main/java/com/ysj/cloudm/domain/monologue/service/MonologueService.java
@@ -2,16 +2,15 @@ package com.ysj.cloudm.domain.monologue.service;
 
 import com.ysj.cloudm.domain.monologue.entity.Monologue;
 import com.ysj.cloudm.domain.monologue.repository.MonologueRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MonologueService {
     private final MonologueRepository monologueRepository;
-    public MonologueService(MonologueRepository monologueRepository) {
-        this.monologueRepository = monologueRepository;
-    }
 
     public Monologue create(Long questionId, String body) {
         Monologue monologue = new Monologue(questionId, body);

21654f23a096f5c66134a25b0413667c69c6e262
harriet221 <potter19@dongguk.edu>
2024-07-24 23:09:33 +0900

    14. 불필요한 리스트 빈 삭제 + 초기 테스트값 추가 + index 메인 페이지 수정

--- src/main/java/com/ysj/cloudm/domain/monologue/repository/MonologueRepository.java
+++ src/main/java/com/ysj/cloudm/domain/monologue/repository/MonologueRepository.java
@@ -4,12 +4,17 @@ import com.ysj.cloudm.domain.monologue.entity.Monologue;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class MonologueRepository {
-    private final List<Monologue> monologues;
+    private final List<Monologue> monologues = new ArrayList<>() {{
+        add(new Monologue(1L, "answer 1"));
+        add(new Monologue(2L, "answer 2"));
+        add(new Monologue(3L, "answer 3"));
+    }};
 
     public Monologue save(Monologue monologue) {
         if(monologue.getId() == null) {
--- src/main/java/com/ysj/cloudm/global/app/AppConfig.java
+++ src/main/java/com/ysj/cloudm/global/app/AppConfig.java
@@ -1,16 +1,7 @@
 package com.ysj.cloudm.global.app;
 
-import com.ysj.cloudm.domain.monologue.entity.Monologue;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    List<Monologue> monologues() {
-        return new ArrayList<>();
-    }
 }

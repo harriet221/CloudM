2a5dfb8d1e903033203b1681ca17493fb6930e63
harriet221 <potter19@dongguk.edu>
2024-11-03 20:00:19 +0900

    39. 세션 json 확인용 메서드 추가

--- src/main/java/com/ysj/cloudm/domain/home/HomeController.java
+++ src/main/java/com/ysj/cloudm/domain/home/HomeController.java
@@ -1,10 +1,15 @@
 package com.ysj.cloudm.domain.home;
 
 import com.ysj.cloudm.global.rq.Rq;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -25,5 +30,18 @@ public class HomeController {
     @GetMapping("/admin/*")
     String showAdmin() {
         return "admin/main";
+    }
+
+    // 세션 확인 용
+    @GetMapping("/session")
+    @ResponseBody
+    public Map<String, Object> showSession(HttpSession session) {
+        return Collections.list(session.getAttributeNames()).stream()
+                .collect(
+                        Collectors.toMap(
+                                key -> key,
+                                key -> session.getAttribute(key)
+                        )
+                );
     }
 }

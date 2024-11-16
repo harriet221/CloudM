3b485b75871e5fb15d6cdb940f17d9139d885cb8
harriet221 <potter19@dongguk.edu>
2024-11-16 23:02:30 +0900

    46-2. sudo 명령어 추가

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 도커 컨테이너 실행
-docker pull harriet99/cloudm-aws-test-ci
-docker run -d -p 8080:8080 harriet99/cloudm-aws-test-ci
+sudo docker pull harriet99/cloudm-aws-test-ci
+sudo docker run -d -p 8080:8080 harriet99/cloudm-aws-test-ci
 
 # 필요한 다른 명령을 추가하세요. 예: Spring Boot 애플리케이션 실행 등
 # 예시:

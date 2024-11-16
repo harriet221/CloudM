23bd62c8e01b172a44d0e8d072c791cb51d535bc
harriet221 <potter19@dongguk.edu>
2024-11-16 22:57:17 +0900

    46. CD 자동 배포 테스트

---
+++ deploy_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 도커 컨테이너 실행
+docker pull harriet99/cloudm-aws-test-ci
+docker run -d -p 8080:8080 harriet99/cloudm-aws-test-ci
+
+# 필요한 다른 명령을 추가하세요. 예: Spring Boot 애플리케이션 실행 등
+# 예시:
+# cd /path/to/your/project
+# java -jar cloudM.jar
